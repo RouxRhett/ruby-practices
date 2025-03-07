5d8a6aad9576aa08d532be3c5f23fe39448a0d70
RouxRhett <nsch.4871@gmail.com>
2025-03-08 00:02:22 +0900

    [change]ループ文修正

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,14 +5,14 @@ require 'date'
 pick_today = Date.today
 
 # 挙動確認
-p pick_today.year
-p pick_today.day
-p pick_today.month
-
+print pick_today.year
+print pick_today.month
+print pick_today.day
+p pick_today.cwday # 初期位置判定に使う
 
 get_month_lastday  = Date.new(pick_today.year, pick_today.month, -1)
-for(1..get_month_lastday.today) do |i|
+(1...get_month_lastday.day).each { |i|
   p i
-end
+}
 
 p get_month_lastday.day
