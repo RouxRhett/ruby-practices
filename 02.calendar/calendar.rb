f27890a65bbce9555d35e9672f368d6ddc054e1b
RouxRhett <nsch.4871@gmail.com>
2025-03-07 23:44:52 +0900

    [change]コメント追加、不要なコード削除、現在for文動かず

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,20 +1,18 @@
 #!/usr/bin/env ruby
+# dateを使うために必要なライブラリ
 require 'date'
-# 
-puts "e"
+
 pick_today = Date.today
+
+# 挙動確認
 p pick_today.year
 p pick_today.day
 p pick_today.month
 
-get_month_firstday = Date.new(pick_today.year, pick_today.month, 1)
+
 get_month_lastday  = Date.new(pick_today.year, pick_today.month, -1)
-show_cal = get_month_lastday - get_month_firstday
+for(1..get_month_lastday.today) do |i|
+  p i
+end
 
-p show_cal
-
-d1 = Date.new(2022, 3, 14)
-d2 = Date.new(2022, 6, 8)
-
-# Rational?から整数に変える
-puts (d2 - d1).class
+p get_month_lastday.day
