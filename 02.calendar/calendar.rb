8906cb1a258b670f5d3ac87d5add3f198f926bc0
RouxRhett <nsch.4871@gmail.com>
2025-03-08 15:32:14 +0900

    [change]カレンダー全体表示の体裁を整える

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -5,10 +5,10 @@ require 'date'
 pick_today = Date.today
 
 # 挙動確認
-print pick_today.year
-print pick_today.month
-print pick_today.day
-p pick_today.cwday # 初期位置判定に使う
+# print pick_today.year
+# print pick_today.month
+# print pick_today.day
+# p pick_today.cwday # 初期位置判定に使う
 init_pos = pick_today.cwday # 初期位置判定に使う
 cur_pos = init_pos # ループ用位置判定に使う
 get_month_lastday  = Date.new(pick_today.year, pick_today.month, -1)
@@ -16,7 +16,7 @@ get_month_lastday  = Date.new(pick_today.year, pick_today.month, -1)
 def day_print(day)
   print sprintf("%2s", day)
 end
-
+puts "      " << pick_today.month.to_s << "月 " << pick_today.year.to_s
 puts "日 月 火 水 木 金 土  "
 
 if(init_pos != 7)
@@ -38,21 +38,21 @@ end
 
 p get_month_lastday.day
 =begin
-❯ cal           
-     March 2025       
-Su Mo Tu We Th Fr Sa  
-                   1  
- 2  3  4  5  6  7  8  
- 9 10 11 12 13 14 15  
-16 17 18 19 20 21 22  
-23 24 25 26 27 28 29  
-30 31              
-❯ cal -m 01
-    January 2025      
-Su Mo Tu We Th Fr Sa  
-          1  2  3  4  
- 5  6  7  8  9 10 11  
-12 13 14 15 16 17 18  
-19 20 21 22 23 24 25  
-26 27 28 29 30 31
+❯ cal -m 12
+      12月 2025
+日 月 火 水 木 金 土
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30 31           
+❯ cal
+      3月 2025
+日 月 火 水 木 金 土
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
 =end
