1b1400588f0c7f306a7e8d983f8dd6ca8832d7b9
RouxRhett <nsch.4871@gmail.com>
2025-03-11 09:09:57 +0900

    [fix]ファイル末尾の改行追加、中がっこの書き換え、ifの後ろにスペース追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 # 1から20までの数をプリントするプログラムを書け。ただし3の倍数のときは数の代わりに｢Fizz｣と、5の倍数のときは｢Buzz｣とプリントし、3と5両方の倍数の場合には｢FizzBuzz｣とプリントすること。
 
-(1..20).each{ |i|
-  if(i % 15 == 0)
+(1..20).each do |i|
+  if (i % 15 == 0)
     puts 'FizzBuzz'
-  elsif(i % 3 == 0)
+  elsif (i % 3 == 0)
     puts 'Fizz'
-  elsif(i % 5 == 0)
+  elsif (i % 5 == 0)
     puts 'Buzz'
   else
     puts i
   end
-}
+end
