87a763f047f8691a607b7c5833c82dae56df9844
RouxRhett <nsch.4871@gmail.com>
2025-03-20 23:42:48 +0900

    [fix]定数用のmoduleを削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -7,19 +7,17 @@ score = ARGV[0]
 scores = score.split(',')
 shots = []
 
-module NORMAL
-  FRAME = 9
-  FRAME_FOR_INDEX = FRAME - 1
-  THROW = 2
-  MAX_THROW = 3
-  TOTAL_MAX_THROW = FRAME * THROW + MAX_THROW
-end
+FRAME = 9
+FRAME_FOR_INDEX = FRAME - 1
+THROW = 2
+MAX_THROW = 3
+TOTAL_MAX_THROW = FRAME * THROW + MAX_THROW
 
 # ストライク分を[10,0]に変換、その他も文字列から値に変換
 scores.each do |s|
   if s == 'X' # strike
     shots << 10
-    shots << nil if shots.size < NORMAL::FRAME * NORMAL::THROW # 1~9F時(1F=2count)
+    shots << nil if shots.size < FRAME * THROW # 1~9F時(1F=2count)
   else
     shots << s.to_i
   end
@@ -28,17 +26,17 @@ end
 # 二次元配列に格納する(1フレーム[1投目,2投目],...),[[6, 3], [9, 0], [0, 3], [8, 2], [7, 3],...
 frames = shots.each_slice(2).to_a
 
-if shots.size == NORMAL::TOTAL_MAX_THROW
+if shots.size == TOTAL_MAX_THROW
   frames[9].push(frames[10]).flatten!
   frames.pop
 end
 
 point = frames.each_with_index.sum do |frame, index|
   score_in_frame = 0
-  if index != NORMAL::FRAME
+  if index != FRAME
     next_first_throw = frames[index + 1][0]
     if frame[0] == 10
-      score_in_frame = if next_first_throw == 10 && index != NORMAL::FRAME_FOR_INDEX
+      score_in_frame = if next_first_throw == 10 && index != FRAME_FOR_INDEX
                          next_first_throw + frames[index + 2][0]
                        else
                          next_first_throw + frames[index + 1][1]
