29902389d86a8c096cba3e80eca576acf5af491d
RouxRhett <nsch.4871@gmail.com>
2025-03-21 10:15:16 +0900

    [fix]変数名を全体的に変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,9 +4,9 @@
 require 'date'
 require 'optparse'
 
-pick_today = Date.today
-show_month = pick_today.month
-show_year  = pick_today.year
+current_date = Date.today
+current_month = current_date.month
+current_year  = current_date.year
 
 opt = OptionParser.new
 
@@ -14,7 +14,7 @@ opt.on('-m [VAL]') do |v|
   if /^[0-9]+$/.match?(v)
     if v.to_i.positive? && v.to_i <= 12
       # p "vは1~12"
-      show_month = v.to_i
+      current_month = v.to_i
     else
       # p "vは1~12ちゃうで"
       puts "#{__FILE__}: #{v} is neither a month number (1..12) nor a name"
@@ -31,7 +31,7 @@ opt.on('-y [VAL]') do |v|
   if /^[0-9]+$/.match?(v)
     if v.to_i.positive? && v.to_i < 10_000
       # p "vは1~9999"
-      show_year = v.to_i
+      current_year = v.to_i
     else
       # p "vは1~9999ちゃうで"
       puts "#{__FILE__}: year `#{v}' not in range 1..9999"
@@ -46,11 +46,11 @@ end
 
 opt.parse!(ARGV)
 
-display_calendar   = Date.new(show_year, show_month, pick_today.day)
+calendar   = Date.new(current_year, current_month, current_date.day)
 
 # 初期位置判定を表示月1日に指定するために定義
-get_month_firstday = Date.new(display_calendar.year, display_calendar.month, 1)
-get_month_lastday  = Date.new(display_calendar.year, display_calendar.month, -1)
+calendar_firstday = Date.new(calendar.year, calendar.month, 1)
+calendar_lastday  = Date.new(calendar.year, calendar.month, -1)
 
 def print_day(date)
   if date == Date.today
@@ -62,15 +62,15 @@ def print_day(date)
   end
 end
 # 式展開に書き方を合わせた
-puts "      #{display_calendar.month}月 #{display_calendar.year}"
+puts "      #{calendar.month}月 #{calendar.year}"
 puts '日 月 火 水 木 金 土  '
 
-get_month_firstday.wday.times do
+calendar_firstday.wday.times do
   print '   '
 end
 
 
-(get_month_firstday..get_month_lastday).each do |date|
+(calendar_firstday..calendar_lastday).each do |date|
   print_day(date)
   if date.saturday?
     puts '  '
